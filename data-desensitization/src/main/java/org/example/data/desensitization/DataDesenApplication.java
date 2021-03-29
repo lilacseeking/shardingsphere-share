14dcaeca1790a96147360fb19e069c20680b0ffe
lilacseeking <lilacseeking.@outlook.com>
2021-03-29 20:17:18 +0800

    gitignore修改

---
+++ data-desensitization/src/main/java/org/example/data/desensitization/DataDesenApplication.java
@@ -0,0 +1,28 @@
+package org.example.data.desensitization;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+@EntityScan({"org.example.*"})
+@ComponentScan(basePackages = {"org.example.*"})
+@EnableDiscoveryClient
+@EnableAspectJAutoProxy
+@EnableDubbo(scanBasePackages = "org.example.*")
+public class DataDesenApplication {
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(DataDesenApplication.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ data-desensitization/src/main/java/org/example/data/desensitization/MainController.java
@@ -0,0 +1,50 @@
+package org.example.data.desensitization;
+
+import org.example.common.model.entity.ShareOrderInfoPO;
+import org.example.common.model.service.OrderService;
+import org.example.data.desensitization.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
+
+@RestController
+@RequestMapping(value = "/order")
+public class MainController {
+
+    @Autowired
+    private ResponseUtil responseUtil;
+
+    @Autowired
+    private OrderService orderService;
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public void insert() {
+        for (int i = 0; i < 1; i++) {
+            ShareOrderInfoPO shareOrderInfoPO = new ShareOrderInfoPO();
+            shareOrderInfoPO.setDataId(new Random().nextInt(999999));
+            shareOrderInfoPO.setTableId(new Random().nextInt(999999));
+            shareOrderInfoPO.setUserId("lilacseeking");
+            shareOrderInfoPO.setOrderNo("2021032806215948" + new Random().nextInt(6));
+            shareOrderInfoPO.setPayId("18803838075");
+            shareOrderInfoPO.setSellerId("18803838000");
+            shareOrderInfoPO.setOrderStatus("INIT");
+            shareOrderInfoPO.setFinishTime(null);
+            shareOrderInfoPO.setTradeMethod("WECHAT");
+            shareOrderInfoPO.setDescription("测试下单");
+            shareOrderInfoPO.setTradeId("2021032806215948" + new Random().nextInt(6));
+            orderService.saveOrUpdate(shareOrderInfoPO);
+        }
+    }
+    @RequestMapping(value = "/query", method = RequestMethod.POST)
+    public void query(Long id, HttpServletResponse res){
+        ShareOrderInfoPO shareOrderInfoPO = new ShareOrderInfoPO();
+        shareOrderInfoPO.setId(id);
+        ShareOrderInfoPO orderInfo = orderService.getOrderInfo(shareOrderInfoPO);
+        responseUtil.putSuccess(orderInfo);
+        responseUtil.writeMessage(res);
+    }
+}
